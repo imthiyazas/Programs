769ab9767c68cf104cb476c6e960bae71901da5f
imthiyazas <35129819+imthiyazas@users.noreply.github.com>
2019-07-27 10:36:31 +0530

    Create String comparison.py

---
+++ String comparison.py
@@ -0,0 +1,16 @@
+str1=input()
+str2=input()
+'''for i in str1:
+    for j in str2:
+        if str1[i] == str2[j]:
+            print("Strings are equal")
+        else:
+            print("Strings are not equal")'''
+if str1 == str2:
+    print("String is equal")
+else:
+    print("string is not equal")
+'''print(id(str1))
+print(id(str2))'''
+
+    
